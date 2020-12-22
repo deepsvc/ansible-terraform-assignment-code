4a83156947b7d563171dad330a9a8b531bc7cb21
Deepak Choudhary <deepak@flux7.com>
2020-12-22 19:39:36 +0530

    updated files

--- config.tf
+++
@@ -1,3 +0,0 @@
-bucket = "test-bucket-terraform-project"
-key    = "network/terraform.tfstate"
-region = "ap-south-1"
--- config2.tf
+++
@@ -1,3 +0,0 @@
-bucket = "test-bucket-terraform-project"
-key    = "network2/terraform.tfstate"
-region = "ap-south-1"
